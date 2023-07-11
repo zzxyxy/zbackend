01424772edbc1f185abee46763e8af8067c9e3d6
zxyxy <zxyxy@protonmail.com>
2023-07-11 09:15:08 +0200

    remove mqtt connection attempt

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ $builder = new DI\ContainerBuilder();
 #$builder->setDefinitionCache(new ApcCache());
 $container = $builder->build();
 $container->set('log\Logging', \log\Log::getInstance());
-$container->set(
-    'zxyxy\mqtt\Imqtt',
-    new zxyxy\mqtt\Mqtt('zxyxyhome.duckdns.org', 1883, "iot", "RyzYK8G53ZCnOR1OKlYL28yNoOagKG")
-);
-
 
 $app = AppFactory::create();
 
